83858b8b3b6d522211f960b8f5eade7b0bd07ecb
bimacahyosaktiutomo <bimasakticahyoutomo@gmail.com>
2023-11-16 22:54:26 +0800

    db

--- ifandi_database.sql
+++ ifandi_database.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 16, 2023 at 05:37 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Waktu pembuatan: 16 Nov 2023 pada 15.53
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `rating`
+-- Struktur dari tabel `rating`
 --
 
 CREATE TABLE `rating` (
@@ -35,21 +35,10 @@ CREATE TABLE `rating` (
   `komentar` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `rating`
---
-
-INSERT INTO `rating` (`id_rating`, `uid`, `id_resep`, `rating`, `komentar`) VALUES
-(3, 3, 2, 3, 'kayak asu'),
-(4, 3, 3, 3, 'kelass'),
-(6, 4, 2, 5, 'kayak ifandi'),
-(7, 5, 2, 5, ''),
-(8, 3, 4, 5, 'wow rasanya sungguh seperto mmk\r\n');
-
 -- --------------------------------------------------------
 
 --
--- Table structure for table `resep`
+-- Struktur dari tabel `resep`
 --
 
 CREATE TABLE `resep` (
@@ -62,21 +51,10 @@ CREATE TABLE `resep` (
   `gambar` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `resep`
---
-
-INSERT INTO `resep` (`id_resep`, `uid`, `nama_resep`, `deskripsi`, `instruksi`, `bahan`, `gambar`) VALUES
-(2, 3, 'Nasi Goreng', 'Nasi dengan cita rasa bumbu ala tenggarong yang diambli dari resep leluhur chef Ifandi', '- Nikahkan Ayam dan Ifandi\r\n- Buat anak\r\n- titit', '- Ifandi\r\n-Ayam', '2023-11-15 10-16-55.Nasi Goreng.jpg'),
-(3, 3, 'Roti', 'Itu pastel anjay', '', '', '2023-11-14 09-49-02.Roti.jpg'),
-(4, 1, 'MMK', 'yang tau tau aja', '', '', 'NoImage.png'),
-(5, 1, 'logo', 'Tampang Rupawan Chef', '', '', 'ifandi.jpg'),
-(6, 1, 'lasagna', 'Makanan favorit kucing oren gendut', '', '', 'NoImage.png');
-
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Struktur dari tabel `user`
 --
 
 CREATE TABLE `user` (
@@ -87,22 +65,11 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`uid`, `username`, `email`, `password`) VALUES
-(1, 'admin', 'admin@gmail.com', '$2y$10$V8k9G383svAvc0JZVOAqpO0OOL2Yh9.eoF4dVQNSPwGiWkxxqkrRG'),
-(2, 'Aldi', 'aldisolihin12@gmail.com', '$2y$10$62wAyDSsNzKVqOVsrieTPu5mYXhQQzbMjnS4kC55OJ/K0nbU6Y5sa'),
-(3, 'ifandi', 'ifandi@gmail.com', '$2y$10$NADZES63/XLCSxCk43ya7eqMBx6hQJalYy6gMnhW6dm4xNZYtd0.S'),
-(4, 'bima', 'bima@gmail.com', '$2y$10$2eON/Kd80xwGGuwTu/kN4ezsazglHfspsA5EwwFzkjkc8p2XTyEzW'),
-(5, 'bima2', 'bima2@gmail.com', '$2y$10$mTWYcuyIGdx3kd.Qt4f68esdVRXguMR/SH.yZVMVN5mHhaK8kIv7u');
-
---
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `rating`
+-- Indeks untuk tabel `rating`
 --
 ALTER TABLE `rating`
   ADD PRIMARY KEY (`id_rating`),
@@ -110,53 +77,53 @@ ALTER TABLE `rating`
   ADD KEY `id_resep` (`id_resep`);
 
 --
--- Indexes for table `resep`
+-- Indeks untuk tabel `resep`
 --
 ALTER TABLE `resep`
   ADD PRIMARY KEY (`id_resep`),
   ADD KEY `uid` (`uid`);
 
 --
--- Indexes for table `user`
+-- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`uid`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `rating`
+-- AUTO_INCREMENT untuk tabel `rating`
 --
 ALTER TABLE `rating`
-  MODIFY `id_rating` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_rating` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `resep`
+-- AUTO_INCREMENT untuk tabel `resep`
 --
 ALTER TABLE `resep`
-  MODIFY `id_resep` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_resep` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- Constraints for dumped tables
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Constraints for table `rating`
+-- Ketidakleluasaan untuk tabel `rating`
 --
 ALTER TABLE `rating`
   ADD CONSTRAINT `rating_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `rating_ibfk_2` FOREIGN KEY (`id_resep`) REFERENCES `resep` (`id_resep`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `resep`
+-- Ketidakleluasaan untuk tabel `resep`
 --
 ALTER TABLE `resep`
   ADD CONSTRAINT `resep_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE;
